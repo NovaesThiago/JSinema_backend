09ae3e198d8314a9e4652b0dc7e1e34d9df67230
NovaesThiago <novaesthiagoss@gmail.com>
2025-08-27 02:41:59 -0300

    Commit case 5 - FiltrarGenero

--- src/watchlist.js
+++ src/watchlist.js
@@ -86,6 +86,25 @@ async function main() {
         console.log("Encerrando programa.");
         break;
 
+      case "5":
+        const generoFiltro = await rl.question("Digite o gênero para filtrar: ");
+        try {
+          const filmes = await WatchlistDAO.listar();
+          const filtrados = filmes.filter(f => f.genero.toLowerCase() === generoFiltro.toLowerCase());
+
+          if (filtrados.length === 0) {
+            console.log("Nenhum filme encontrado com esse gênero.");
+          } else {
+            console.log("\nFilmes encontrados:");
+            filtrados.forEach(f => console.log(`${f.id} - ${f.titulo} (${f.genero})`));
+          }
+        } catch (error) {
+          console.error("Erro ao filtrar por gênero:", error.message);
+        }
+        break;
+        
+      
+
       default:
         console.log("Opção inválida. Tente novamente.");
     }
